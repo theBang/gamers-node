dbc62cba4da79d74f16d3a0294d91846e6082f11
Danil <d.danil.s.96@gmail.com>
2023-03-16 19:15:07 +0500

    change parse date from js to sql

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { promises: fs } = require("fs");
 const { parse } = require("csv-parse");
 const { Client } = require("pg");
+const format = require('pg-format');
 const head = require("./data/head.json")
 
 async function parseCsv(filePath) {
@@ -14,47 +15,44 @@ async function parseCsv(filePath) {
     });
 }
 
-function parseDate(dateString) {
-    const dateParts = dateString.split(" ");
-    const date = dateParts[0].split(".");
-    const time = dateParts[1].split(":");
-    const dateUTC = Date.UTC(date[2], date[1] - 1, date[0], time[0], time[1]);
-
-    // Move from ms to seconds precision to fit in integer
-    return dateUTC / 1000; 
-}
-
 (async function fillTable() {
     const client = new Client();
     await client.connect();
 
     const tableName = "players";
-    const createTableText = `
-    CREATE TEMP TABLE IF NOT EXISTS ${tableName} (
+    const createTableText = format(`
+    CREATE TEMP TABLE IF NOT EXISTS %I (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         nickname character varying(64) UNIQUE NOT NULL,
         email character varying(320) UNIQUE NOT NULL,
         registered integer NOT NULL,
         status boolean NOT NULL
     );
-    `;
+    `, tableName);
+    console.log(createTableText);
     await client.query(createTableText);
 
-    const insertText = `INSERT INTO ${tableName}(nickname, email, registered, status) VALUES($1, $2, $3, $4)`;
     const records = await parseCsv(path.resolve("data", "players.csv"));
 
     for await (const record of records) {
         console.log(record);
-        const values = [
-            record[head.NICKNAME], 
-            record[head.EMAIL], 
-            parseDate(record[head.REGISTERED]), 
+        const insertText = format(`
+        INSERT INTO %I(nickname, email, registered, status) VALUES(
+            %L, 
+            %L, 
+            cast(extract(epoch from to_timestamp(%L, 'DD.MM.YYYY HH24:MI')::timestamp) as integer), 
+            %L
+        );
+        `, tableName,
+            record[head.NICKNAME],
+            record[head.EMAIL],
+            record[head.REGISTERED],
             record[head.STATUS] === head.ON
-        ];
-        await client.query(insertText, values);
+        );
+        await client.query(insertText);
     }
 
-    const { rows } = await client.query(`SELECT * FROM ${tableName}`);
+    const { rows } = await client.query(format("SELECT * FROM %I", tableName));
     console.log(rows);
 
     await client.end();
