dab9391198d33937263b6aa43c50a42c5e829cb5
Danil <d.danil.s.96@gmail.com>
2023-03-16 18:24:54 +0500

    add simple csv extract

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 require("dotenv").config();
+const path = require("path");
+const { promises: fs } = require("fs");
+const { parse } = require("csv-parse");
 const { Client } = require("pg");
 
-(async () => {
+const NICKNAME = "Ник";
+const EMAIL = "Email";
+const REGISTERED = "Зарегистрирован";
+const STATUS = "Статус";
+const ON = "ON";
+
+async function parseCsv(filePath) {
+    const content = await fs.readFile(filePath);
+    return parse(content, {
+        delimiter: ";",
+        trim: true,
+        columns: true
+    });
+}
+
+function parseDate(dateString) {
+    const dateParts = dateString.split(" ");
+    const date = dateParts[0].split(".");
+    const time = dateParts[1].split(":");
+    const dateUTC = Date.UTC(date[2], date[1] - 1, date[0], time[0], time[1]);
+
+    // Move from ms to seconds precision to fit in integer
+    return dateUTC / 1000; 
+}
+
+(async function fillTable() {
     const client = new Client();
     await client.connect();
 
+    const tableName = "players";
     const createTableText = `
-    CREATE TABLE IF NOT EXISTS players (
+    CREATE TEMP TABLE IF NOT EXISTS ${tableName} (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         nickname character varying(64) UNIQUE NOT NULL,
         email character varying(320) UNIQUE NOT NULL,
@@ -16,12 +45,21 @@ const { Client } = require("pg");
     `;
     await client.query(createTableText);
 
-    const insertText = "INSERT INTO players(nickname, email, registered, status) VALUES($1, $2, $3, $4)"
-    const values = ["ivan", "ivan@mail.ru", 123, true];
-    await client.query(insertText, values);
+    const insertText = `INSERT INTO ${tableName}(nickname, email, registered, status) VALUES($1, $2, $3, $4)`;
+    const records = await parseCsv(path.resolve("data", "players.csv"));
 
-    const { rows } = await client.query("SELECT * FROM players");
+    for await (const record of records) {
+        console.log(record);
+        const values = [
+            record[NICKNAME], 
+            record[EMAIL], 
+            parseDate(record[REGISTERED]), 
+            record[STATUS] === ON];
+        await client.query(insertText, values);
+    }
+
+    const { rows } = await client.query(`SELECT * FROM ${tableName}`);
     console.log(rows);
 
     await client.end();
-})()
+})();
